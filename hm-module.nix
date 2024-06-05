2cd0033f28712431e2f14460daee0caeaaf937b2
Raphael Groemmer <mail@rapsn.me>
2024-06-05 14:47:13 +0200

    Add common options, activation for hm

--- hm-module.nix
+++ hm-module.nix
@@ -1,18 +1,47 @@
 self:
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
 with lib;
+
 let
   cfg = config.programs.krewfile;
   finalPackage = self.packages.${pkgs.system}.krewfile;
+  krewfile = pkgs.writeText "krewfile" ''
+    ${cfg.plugins}
+  '';
 in
 {
   options.programs.krewfile = {
     enable = mkEnableOption "krewfile";
+
     installKrew = mkEnableOption "installKrew";
+
+    krewPackage = mkOption {
+      type = types.package;
+      default = pkgs.krew;
+      defaultText = literalExpression "pkgs.krew";
+      description = "krew package to install.";
+    };
+
+    plugins = mkOption { type = with types; listOf str; };
   };
+
   config = mkIf cfg.enable {
-    home.packages = ([
-      finalPackage
-    ] ++ optionals cfg.installKrew [ pkgs.krew ]);
+
+    home.packages = ([ finalPackage ] ++ optionals cfg.installKrew [ cfg.krewPackage ]);
+
+    home.file.".krewfile".text = ''
+      ${concatStringsSep "\n" cfg.plugins}
+    '';
+
+    home.activation.krew = hm.dag.entryAfter [ "writeBoundary" ] ''
+      PATH=$PATH:${cfg.krewPackage}/bin
+      run ${finalPackage}/bin/${finalPackage.pname}
+    '';
   };
 }
