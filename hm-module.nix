5667025d858705912723461f4cc935a5ae254658
Raphael Groemmer <mail@rapsn.me>
2024-06-05 17:29:13 +0200

    install krew as default, add env vars

--- hm-module.nix
+++ hm-module.nix
@@ -12,38 +12,47 @@ let
   cfg = config.programs.krewfile;
   finalPackage = self.packages.${pkgs.system}.krewfile;
   krewfileContent = pkgs.writeText "krewfile" (concatStringsSep "\n" cfg.plugins);
+  args = if cfg.upgrade then "-upgrade" else "";
 in
 {
   options.programs.krewfile = {
     enable = mkEnableOption "krewfile";
 
-    installKrew = mkEnableOption "installKrew";
-
     krewPackage = mkOption {
       type = types.package;
       default = pkgs.krew;
       defaultText = literalExpression "pkgs.krew";
-      description = ''
-        Krew package to install.
-        Only relevant if `programs.krewfile.installKrew` is enabled.
-      '';
+      description = "Krew package to install.";
     };
 
     plugins = mkOption {
       type = with types; listOf str;
       default = [ ];
-      description = "List of plugins to be written to the krewfile.";
+      defaultText = literalExpression "[ "edit-status" ]";
+      description = "List of plugins to be installed.";
+    };
+
+    upgrade = mkOption {
+      type = types.bool;
+      default = false;
+      defaultText = literalExpression "false";
+      description = "Enable auto update of plugins.";
     };
   };
 
   config = mkIf cfg.enable {
 
-    home.packages = ([ finalPackage ] ++ optionals cfg.installKrew [ cfg.krewPackage ]);
+    home.packages = [ finalPackage cfg.krewPackage ];
+
+    home.sessionVariables.PATH = "$HOME/.krew/bin:$PATH";
 
     home.activation.krew = hm.dag.entryAfter [ "writeBoundary" ] ''
+      echo $PATH
+      PATH="$HOME/.krew/bin:$PATH"
+      run ${cfg.krewPackage}/bin/${cfg.krewPackage.pname} update
       run ${finalPackage}/bin/${finalPackage.pname} \
         -command ${cfg.krewPackage}/bin/${cfg.krewPackage.pname} \
-        -file ${krewfileContent} -upgrade
+        -file ${krewfileContent} ${args}
     '';
   };
 }
