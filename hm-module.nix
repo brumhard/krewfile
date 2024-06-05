f7afb1b819bbb9acd824c355469cd958bd58e3a9
Raphael Groemmer <mail@rapsn.me>
2024-06-05 15:14:00 +0200

    add more readability, nixfmt

--- hm-module.nix
+++ hm-module.nix
@@ -11,9 +11,7 @@ with lib;
 let
   cfg = config.programs.krewfile;
   finalPackage = self.packages.${pkgs.system}.krewfile;
-  krewfile = pkgs.writeText "krewfile" ''
-    ${cfg.plugins}
-  '';
+  krewfileContent = concatStringsSep "\n" cfg.plugins;
 in
 {
   options.programs.krewfile = {
@@ -25,19 +23,36 @@ in
       type = types.package;
       default = pkgs.krew;
       defaultText = literalExpression "pkgs.krew";
-      description = "krew package to install.";
+      description = ''
+        Krew package to install.
+        Only relevant if `programs.krewfile.installKrew` is enabled.
+      '';
     };
 
-    plugins = mkOption { type = with types; listOf str; };
+    path = mkOption {
+      type = types.string;
+      default = ".krewfile";
+      defaultText = literalExpression ".krewfile";
+      description = ''
+        Specify the path where the `krewfile` is written.
+        This is relative to the home directory.
+      '';
+    };
+
+    plugins = mkOption {
+      type = with types; listOf str;
+      default = [ ];
+      description = "List of plugins to be written to the krewfile.";
+    };
   };
 
   config = mkIf cfg.enable {
 
     home.packages = ([ finalPackage ] ++ optionals cfg.installKrew [ cfg.krewPackage ]);
 
-    home.file.".krewfile".text = ''
-      ${concatStringsSep "\n" cfg.plugins}
-    '';
+    home.file = {
+      ${cfg.path}.text = krewfileContent;
+    };
 
     home.activation.krew = hm.dag.entryAfter [ "writeBoundary" ] ''
       PATH=$PATH:${cfg.krewPackage}/bin
